c4cdd3de4028bae9f81f875ed6708790873fa91a
Ruben-pergra <ruben.pergra05@gmail.com>
2025-01-08 22:45:34 +0100

    Añadida interfaz genérica Dict

--- Dict.h
+++ Dict.h
@@ -2,12 +2,14 @@
 #define DICT_H
 #include <string>
 
+using namespace std;
+
 template <typename V> 
 class Dict {
     public:
-        virtual void insert(std::string key, V value) = 0;
-	virtual V search(std::string key) = 0;
-	virtual V remove(std::string key) = 0;
+    virtual void insert(string key, V value) = 0;
+	virtual V search(string key) = 0;
+	virtual V remove(string key) = 0;
 	virtual int entries() = 0;
 };
 
